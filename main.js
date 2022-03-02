5c122a9e645493f0c378c16e975d98c3027a4fc9
Mario Querol <marioquerolamondarain@gmail.com>
2022-03-02 11:40:47 +0100

    Se termina la funcionalidad básica y se añade presentación

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/* 
+    TAREAS
+*/
 const tareas = [];
 const estados = ["todo", "doing", "done", "deleted"];
 
@@ -12,15 +15,7 @@ for (let index = 0; index < 8; index++)
 }
 console.log("Se crean las tareas");
 
-const tareaTemplate = document.getElementById("tareaGeneral");
-const listaGeneral = document.getElementById("listaGeneral");
-const listaToDo = document.getElementById("listaToDo");
-const listaDoing = document.getElementById("listaDoing");
-const listaDone = document.getElementById("listaDone");
-const listaDeleted = document.getElementById("listaDeleted");
-const tareasNodos = [];
 
-listaGeneral.removeChild(listaGeneral.firstChild);
 tareas.forEach(tarea => {
     actualizarTareas(tarea);
 });
@@ -28,54 +23,127 @@ listaGeneral.removeChild(listaGeneral.firstChild);
 console.log("Se añaden las tareas a las respectivas listas");
 
 /* 
-    EVENTOS BOTONES
+    FUNCIONES
 */
-document.getElementById("btnAñadirTarea").addEventListener('click', (e) => {
-    e.preventDefault();
-    let nombre = document.getElementById("nombreTarea").value;
-    let descripcion = document.getElementById("descripcionTarea").value;
-    let estado = document.getElementById("estadoTarea").value;
-    let prioridad = document.getElementById("prioridadTarea").value;
+function ocultar(lista)
+{
+    if(lista == "general")
+    {
+        document.getElementById("listaGeneralContenedor").style.display = "none";
+        document.getElementById("mostrarCreadas").style.display = "";
+    }
+    else
+    {
+        document.getElementById("listaDeletedContenedor").style.display = "none";
+        document.getElementById("mostrarEliminadas").style.display = "";
 
-    let nuevaTarea = new Task(nombre, descripcion, estado, prioridad, id++);
-    actualizarTareas(nuevaTarea);
-    tareas.push(nuevaTarea);
-    console.log("Se añade nueva tarea, con id: " + id);
-});;
+    }
+    console.log("Se oculta " + lista + " se muestra botón para desocultar");
+}
+function mostrar(lista)
+{
+    if(lista == "general")
+    {
+        document.getElementById("listaGeneralContenedor").style.display = "";
+        document.getElementById("mostrarCreadas").style.display = "none";
+    }
+    else
+    {
+        document.getElementById("listaDeletedContenedor").style.display = "";
+        document.getElementById("mostrarEliminadas").style.display = "none";
+    }
+    console.log("Se muestra " + lista + " se oculta botón para desocultar");
+}
 
-document.getElementById("ocultarGeneral").addEventListener('click', (e) => {
-    ocultar("general");
-});
-document.getElementById("mostrarCreadas").style.display = "none";
-document.getElementById("mostrarCreadas").addEventListener('click', (e) => {
-    mostrar("general");
-});
+function actualizarTareas(tarea)
+{
+    let tareaItem = tareaTemplate.cloneNode(true);
+    tareaItem.setAttribute('id', tarea.id);
 
-document.getElementById("ocultarDeleted").addEventListener('click', (e) => {
-    ocultar("deleted");
-});
-document.getElementById("mostrarEliminadas").style.display = "none";
-document.getElementById("mostrarEliminadas").addEventListener('click', (e) => {
-    mostrar("deleted");
-});
+    let tituloTarea = tareaItem.getElementsByClassName("tituloTarea");
+    tituloTarea[0].innerHTML = tarea.name + " [" + tarea.priority + "]";
+    
+    let descripcionTarea = tareaItem.getElementsByClassName("textoTarea");
+    descripcionTarea[0].innerHTML = tarea.description;
 
-const nombreBotones = ["ToDo", "Doing", "Done"];
-nombreBotones.forEach(nombre => {
-    document.getElementById("vaciar" + nombre).addEventListener('click', (e) => {
-        vaciarLista(nombre.toLowerCase());
-    });    
+    let estadoTarea = tareaItem.getElementsByClassName("estadoTarea");
+    estadoTarea[0].innerHTML = tarea.state;
+    
+    if(tarea.state === "todo"){
+        estadoTarea[0].style.color = "yellow";
+        listaToDo.appendChild(tareaItem.cloneNode(true));
+    } else if(tarea.state === "doing") {
+        estadoTarea[0].style.color = "blue";
+        listaDoing.appendChild(tareaItem.cloneNode(true));
+    } else if(tarea.state === "done") {    
+        estadoTarea[0].style.color = "#1E7116";
+        listaDone.appendChild(tareaItem.cloneNode(true));
+    } else {// if(tarea.state == "deleted")
+        estadoTarea[0].style.color = "red";
+        listaDeleted.appendChild(tareaItem.cloneNode(true));
+    }
 
-    document.getElementById("ordenarAscendente" + nombre).addEventListener('click', (e) => {
-        ordenarLista(nombre.toLowerCase(), true);
-    });
+    listaGeneral.appendChild(tareaItem.cloneNode(true));
+    indice++;
+}
+function vaciarLista(lista)
+{
+    var confirmacion = confirm("Desea eliminar la lista " + lista);
+    if(!confirmacion){
+        return;
+    }
 
-    document.getElementById("ordenarDescendente" + nombre).addEventListener('click', (e) => {
-        ordenarLista(nombre.toLowerCase(), false);
-    });
-});
+    for (let indice = 0; indice < tareas.length; indice++) 
+    {
+        let tarea = tareas[indice];
+        if(tareas[indice].state == lista)
+        {
+            tareas[indice].state = "deleted";
+            let tareaActualizada = tareas[indice];
+            eliminarTarea(tarea.id);
+            actualizarTareas(tareaActualizada);
+
+            console.log("Actualizada " + tareas[indice].name + " Estado: " + tareas[indice].state);
+        }
+    }
+
+    if(lista == "todo"){
+        listaToDo.innerHTML = "";
+    } else if( lista == "doing") {
+        listaDoing.innerHTML = "";
+    } else if(lista == "done") {  
+        listaDone.innerHTML = "";  
+    } else if(lista == "deleted") {
+        listaDeleted.innerHTML = "";
+    } else {
+        console.log("Lista no válida");
+    }
+
+    console.log("Lista " + lista + " vaciada");
+}
+function eliminarTarea(tareaId)
+{
+    let hijos = listaGeneral.getElementsByClassName("itemTarea");
+    
+    let eliminada = false;
+    for(let i = 0; i < hijos.length; i++)
+    {
+        if(hijos[i].getAttribute("id") == tareaId)
+        {
+            listaGeneral.removeChild(hijos[i]);
+            eliminada = true;
+        }
+    }
+
+    if(eliminada){
+        console.log("Tarea con id " + tareaId + " ha sido eliminada");
+    }else {
+        console.log("No existe la tarea con id " + tareaId);
+    }
+}
 
 /* 
-    FUNCIONES
+    EN DESARROLLO
 */
 function ordenarLista(lista, ascendente)
 {
@@ -128,11 +196,8 @@ function ordenarLista(lista, ascendente)
     else
         console.log("ToDo"); */
 }
-
 function obtenerPrioridad(hijos, nElemento)
 {
-    /* let hijos = lista.getElementsByClassName("itemTarea"); */
-    
     let titulo = hijos[nElemento].getElementsByClassName("tituloTarea")[0].innerHTML;
 
     let indice;
@@ -144,124 +209,4 @@ function obtenerPrioridad(hijos, nElemento)
         }
     }
     return titulo.substring(indice+1, titulo.length-1);
-}
-
-function ocultar(lista)
-{
-    if(lista == "general")
-    {
-        document.getElementById("listaGeneralContenedor").style.display = "none";
-        document.getElementById("mostrarCreadas").style.display = "";
-    }
-    else
-    {
-        document.getElementById("listaDeletedContenedor").style.display = "none";
-        document.getElementById("mostrarEliminadas").style.display = "";
-
-    }
-    console.log("Se oculta " + lista + " se muestra botón para desocultar");
-}
-function mostrar(lista)
-{
-    if(lista == "general")
-    {
-        document.getElementById("listaGeneralContenedor").style.display = "";
-        document.getElementById("mostrarCreadas").style.display = "none";
-    }
-    else
-    {
-        document.getElementById("listaDeletedContenedor").style.display = "";
-        document.getElementById("mostrarEliminadas").style.display = "none";
-    }
-    console.log("Se muestra " + lista + " se oculta botón para desocultar");
-}
-
-function actualizarTareas(tarea)
-{
-    let tareaItem = tareaTemplate.cloneNode(true);
-    tareaItem.setAttribute('id', tarea.id);
-
-    let tituloTarea = tareaItem.getElementsByClassName("tituloTarea");
-    tituloTarea[0].innerHTML = tarea.name + " [" + tarea.priority + "]";
-    
-    let descripcionTarea = tareaItem.getElementsByClassName("textoTarea");
-    descripcionTarea[0].innerHTML = tarea.description;
-
-    let estadoTarea = tareaItem.getElementsByClassName("estadoTarea");
-    estadoTarea[0].innerHTML = tarea.state;
-    
-    if(tarea.state === "todo"){
-        estadoTarea[0].style.color = "yellow";
-        listaToDo.appendChild(tareaItem.cloneNode(true));
-    } else if(tarea.state === "doing") {
-        estadoTarea[0].style.color = "blue";
-        listaDoing.appendChild(tareaItem.cloneNode(true));
-    } else if(tarea.state === "done") {    
-        estadoTarea[0].style.color = "green";
-        listaDone.appendChild(tareaItem.cloneNode(true));
-    } else {// if(tarea.state == "deleted")
-        estadoTarea[0].style.color = "red";
-        listaDeleted.appendChild(tareaItem.cloneNode(true));
-    }
-
-    listaGeneral.appendChild(tareaItem.cloneNode(true));
-    indice++;
-    tareasNodos.push(tareaItem);
-}
-
-function eliminarTarea(tareaId)
-{
-    let hijos = listaGeneral.getElementsByClassName("itemTarea");
-    
-    let eliminada = false;
-    for(let i = 0; i < hijos.length; i++)
-    {
-        if(hijos[i].getAttribute("id") == tareaId)
-        {
-            listaGeneral.removeChild(hijos[i]);
-            eliminada = true;
-        }
-    }
-
-    if(eliminada){
-        console.log("Tarea con id " + tareaId + " ha sido eliminada");
-    }else {
-        console.log("No existe la tarea con id " + tareaId);
-    }
-}
-
-function vaciarLista(lista)
-{
-    var confirmacion = confirm("Desea eliminar la lista " + lista);
-    if(!confirmacion){
-        return;
-    }
-
-    for (let indice = 0; indice < tareas.length; indice++) 
-    {
-        let tarea = tareas[indice];
-        if(tareas[indice].state == lista)
-        {
-            tareas[indice].state = "deleted";
-            let tareaActualizada = tareas[indice];
-            eliminarTarea(tarea.id);
-            actualizarTareas(tareaActualizada);
-
-            console.log("Actualizada " + tareas[indice].name + " Estado: " + tareas[indice].state);
-        }
-    }
-
-    if(lista == "todo"){
-        listaToDo.innerHTML = "";
-    } else if( lista == "doing") {
-        listaDoing.innerHTML = "";
-    } else if(lista == "done") {  
-        listaDone.innerHTML = "";  
-    } else if(lista == "deleted") {
-        listaDeleted.innerHTML = "";
-    } else {
-        console.log("Lista no válida");
-    }
-
-    console.log("Lista " + lista + " vaciada");
 }
